bf2bc1d76acfa6d13b464c8b5ef446be76ef7c31
Biradar1 <namdeb10@gmail.com>
2019-06-05 04:53:29 +0530

    first commit in github

---
+++ src/gitexample/Gitexample.java
@@ -0,0 +1,13 @@
+package gitexample;
+
+public class Gitexample 
+{
+	public static void main(String args[])
+	{
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println("Wel Com to the githum");
+		}
+	}
+
+}
